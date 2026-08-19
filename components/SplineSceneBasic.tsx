736015e43a8bd294c9eb66303c501072d0b5d1ed
Francis Obi <163821994+justatomic@users.noreply.github.com>
2026-08-19 15:48:11 +0100

    Update SplineSceneBasic.tsx

--- components/SplineSceneBasic.tsx
+++ components/SplineSceneBasic.tsx
@@ -9,11 +9,9 @@ export function SplineSceneBasic() {
     <Card className="w-full h-[500px] bg-black/[0.96] relative overflow-hidden">
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
-        fill="white"
       />
       
       <div className="flex h-full">
-        {/* Left content */}
         <div className="flex-1 p-8 relative z-10 flex flex-col justify-center">
           <h1 className="text-4xl md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
             Interactive 3D
@@ -24,7 +22,6 @@ export function SplineSceneBasic() {
           </p>
         </div>
 
-        {/* Right content */}
         <div className="flex-1 relative">
           <SplineScene 
             scene="https://prod.spline.design/kZDDjO5HuC9GJUM2/scene.splinecode"
